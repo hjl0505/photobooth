4cb0cdfef63b57732b2ea9a066f8bd4878e50e96
Chris Knakal <cknakal@gmail.com>
2016-11-12 16:48:19 -0800

    Added flip frame

--- WebCamTest.cpp
+++ WebCamTest.cpp
@@ -1,28 +1,1 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <iostream>
-using namespace std;
-using namespace cv;
-
-// From: http://answers.opencv.org/question/1/how-can-i-get-frames-from-my-webcam/
-
-int main(int argc, char** argv)
-{
-	VideoCapture cap;
-	// open the default camera, use something different from 0 otherwise;
-	// Check VideoCapture documentation.
-	if (!cap.open(0))
-		return 0;
-	while (true)
-	{
-		Mat frame;
-		cap >> frame;
-		if (frame.empty()) break; // end of video stream
-		imshow("this is you, smile! :)", frame);
-		if (waitKey(1) == 27) break; // stop capturing by pressing ESC
-	}
-	// the camera will be closed automatically upon exit
-	// cap.close();
-	return 0;
-}
+#include <opencv2/core/core.hpp>#include <opencv2/highgui/highgui.hpp>#include <opencv2/imgproc/imgproc.hpp>#include <iostream>using namespace std;using namespace cv;// From: http://answers.opencv.org/question/1/how-can-i-get-frames-from-my-webcam/int main(int argc, char** argv){	VideoCapture cap;	// open the default camera, use something different from 0 otherwise;	// Check VideoCapture documentation.	if (!cap.open(0))		return 0;	while (true)	{		Mat frame;		cap >> frame;		// end of video stream		if (frame.empty()) break;		// flip the frame for ease of use		Mat flipFrame;		flip(frame, flipFrame, 1);		// project camera images		imshow("this is you, smile! :)", flipFrame);		// stop capturing by pressing ESC		if (waitKey(1) == 27) break;	}	// the camera will be closed automatically upon exit	// cap.close();	return 0;}
